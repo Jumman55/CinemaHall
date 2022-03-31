ca631e1c3148e7800fccc33fe6d15c1aa5ea96f7
Jumman <jumman.1948848@studenti.uniroma1.it>
2022-03-31 21:01:06 +0200

    Initial Commit

---
+++ CinemaHall/CinemaHallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaHallApp.swift
+//  CinemaHall
+//
+//  Created by Jumman Hossen on 31/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CinemaHallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
